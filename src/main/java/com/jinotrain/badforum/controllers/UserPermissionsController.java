5ee089a3718e999963f2dd0ceef38920e5e28859
ijon <brouzoufman@lavabit.com>
2019-10-27 03:44:38 -0500

    "/roles/<username>" merged with "/settings/<username>"

--- src/main/java/com/jinotrain/badforum/controllers/UserPermissionsController.java
+++ src/main/java/com/jinotrain/badforum/controllers/UserPermissionsController.java
@@ -2,8 +2,6 @@ package com.jinotrain.badforum.controllers;
 
 import com.jinotrain.badforum.data.UserPermissionStateData;
 import com.jinotrain.badforum.data.UserRoleData;
-import com.jinotrain.badforum.data.UserRoleStateData;
-import com.jinotrain.badforum.data.UserViewData;
 import com.jinotrain.badforum.db.PermissionState;
 import com.jinotrain.badforum.db.UserPermission;
 import com.jinotrain.badforum.db.entities.ForumRole;
@@ -13,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -51,32 +48,6 @@ public class UserPermissionsController extends ForumController
             }
 
              ret.add(new UserRoleData(role.getName(), role.getPriority(), role.isAdmin(), role.isDefaultRole(), permissions, i++));
-        }
-
-        return ret;
-    }
-
-
-    private List<UserRoleStateData> getUserRoles(ForumUser user)
-    {
-        List<ForumRole> userRoles = user.getRoles();
-        List<UserRoleStateData> ret = new ArrayList<>();
-
-        Set<Long> userRoleIDs = new HashSet<>();
-        for (ForumRole role: userRoles) { userRoleIDs.add(role.getId()); }
-
-
-        List<Object[]> roleData = em.createQuery("SELECT role.name, role.id, role.admin FROM ForumRole role"
-                                               + " WHERE role.defaultRole = false ORDER BY role.priority DESC", Object[].class)
-                                    .getResultList();
-
-        int i = 0;
-
-        for (Object[] d: roleData)
-        {
-            String  name    = (String)d[0];
-            boolean present = userRoleIDs.contains((Long)d[1]);
-            ret.add(new UserRoleStateData(name, present, (boolean)d[2], i++));
         }
 
         return ret;
@@ -154,31 +125,6 @@ public class UserPermissionsController extends ForumController
         ModelAndView ret = new ModelAndView("roles.html");
         ret.addObject("roleData", getRolesAndPerms());
         ret.addObject("permissions", UserPermission.values());
-        return ret;
-    }
-
-
-    @Transactional
-    @RequestMapping("/roles/*")
-    public ModelAndView viewUserRoles(HttpServletRequest request, HttpServletResponse response)
-    {
-        ForumUser accessUser = getUserFromRequest(request);
-
-        if (!userHasPermission(accessUser, UserPermission.MANAGE_USERS))
-        {
-            return errorPage("userroles_error.html", "NOT_ALLOWED", HttpStatus.UNAUTHORIZED);
-        }
-
-        String viewUsername = request.getServletPath().substring("/roles/".length());
-        ForumUser viewUser = userRepository.findByUsernameIgnoreCase(viewUsername);
-
-        if (viewUser == null)
-        {
-            return errorPage("userroles_error.html", "NOT_FOUND", HttpStatus.NOT_FOUND);
-        }
-
-        ModelAndView ret = new ModelAndView("userroles.html");
-        ret.addObject("userdata", new UserViewData(viewUser.getUsername(), getUserRoles(viewUser)));
         return ret;
     }
 
@@ -294,9 +240,14 @@ public class UserPermissionsController extends ForumController
 
 
     @Transactional
-    @RequestMapping(value = "/saveroles", method = RequestMethod.POST)
+    @RequestMapping(value = "/saveroles")
     public ModelAndView saveRoleSettings(HttpServletRequest request, HttpServletResponse response)
     {
+        if (!request.getMethod().equals("POST"))
+        {
+            return errorPage("roles_error.html", "POST_ONLY", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
         ForumUser accessUser = getUserFromRequest(request);
 
         if (!userHasPermission(accessUser, UserPermission.MANAGE_USERS))
@@ -328,9 +279,14 @@ public class UserPermissionsController extends ForumController
     //  <UserPermission.name()>: -1 for off, 0 for keep, 1 for on
 
     @Transactional
-    @RequestMapping(value = "/newrole", method = RequestMethod.POST)
+    @RequestMapping(value = "/newrole")
     public ModelAndView createNewRole(HttpServletRequest request, HttpServletResponse response)
     {
+        if (!request.getMethod().equals("POST"))
+        {
+            return errorPage("roles_error.html", "POST_ONLY", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
         ForumUser user = getUserFromRequest(request);
 
         if (!userHasPermission(user, UserPermission.MANAGE_USERS))
@@ -408,86 +364,6 @@ public class UserPermissionsController extends ForumController
 
         ModelAndView ret = new ModelAndView("newrole.html");
         ret.addObject("roleName", newRole.getName());
-        return ret;
-    }
-
-
-
-    private void updateRolesFromParams(ForumUser user, Map<String, String[]> params) throws IllegalArgumentException
-    {
-        Map<String, Boolean> roleStates = new HashMap<>();
-
-        for (Map.Entry<String, String[]> param: params.entrySet())
-        {
-            String roleName  = param.getKey().toLowerCase();
-            String roleState = param.getValue()[0];
-
-            if (roleName.startsWith("role:")) { roleName = roleName.substring("role:".length()); }
-            else { continue; }
-
-            roleStates.put(roleName, "1".equals(roleState));
-        }
-
-        if (roleStates.isEmpty())
-        {
-            throw new IllegalArgumentException("No roles specified");
-        }
-
-        List<ForumRole> roles = em.createQuery("SELECT role FROM ForumRole role WHERE lower(role.name) IN :roleNames"
-                                             + " AND role.defaultRole = false", ForumRole.class)
-                                        .setParameter("roleNames", roleStates.keySet())
-                                        .getResultList();
-
-        for (ForumRole role: roles)
-        {
-            boolean keep = roleStates.get(role.getName().toLowerCase());
-
-            if (keep) { user.addRole(role); }
-            else      { user.removeRole(role); }
-        }
-    }
-
-
-    // parameter format:
-    //  key: role:[role name]
-    //  value: 0 for "take away this role", 1 for "give this role"
-
-    @Transactional
-    @RequestMapping(value = "/saveuserroles", method = RequestMethod.POST)
-    public ModelAndView saveUserRoles(HttpServletRequest request, HttpServletResponse response)
-    {
-        ForumUser accessUser = getUserFromRequest(request);
-
-        if (!userHasPermission(accessUser, UserPermission.MANAGE_USERS))
-        {
-            return errorPage("userroles_error.html", "NOT_ALLOWED", HttpStatus.UNAUTHORIZED);
-        }
-
-        String modifyUsername = request.getParameter("username");
-
-        if (modifyUsername == null)
-        {
-            return errorPage("userroles_error.html", "MISSING_USERNAME", HttpStatus.BAD_REQUEST);
-        }
-
-        ForumUser modifyUser = userRepository.findByUsernameIgnoreCase(modifyUsername);
-
-        if (modifyUser == null)
-        {
-            return errorPage("userroles_error.html", "NOT_FOUND", HttpStatus.NOT_FOUND);
-        }
-
-        try
-        {
-            updateRolesFromParams(modifyUser, request.getParameterMap());
-        }
-        catch (IllegalArgumentException e)
-        {
-            return errorPage("userroles_error.html", "MISSING_ROLES", HttpStatus.BAD_REQUEST);
-        }
-
-        ModelAndView ret = new ModelAndView("userroles_saved.html");
-        ret.addObject("username", modifyUser.getUsername());
         return ret;
     }
 }
--- src/main/java/com/jinotrain/badforum/controllers/UserSettingsController.java
+++ src/main/java/com/jinotrain/badforum/controllers/UserSettingsController.java
@@ -1,34 +1,28 @@
 package com.jinotrain.badforum.controllers;
 
 import com.jinotrain.badforum.components.passwords.ForumPasswordService;
+import com.jinotrain.badforum.data.UserRoleStateData;
 import com.jinotrain.badforum.data.UserSettingViewData;
+import com.jinotrain.badforum.data.UserViewData;
 import com.jinotrain.badforum.db.UserPermission;
+import com.jinotrain.badforum.db.entities.ForumRole;
 import com.jinotrain.badforum.db.entities.ForumUser;
 import com.jinotrain.badforum.lambdas.UserSettingInterface;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class UserSettingsController extends ForumController
 {
-    private static Logger logger = LoggerFactory.getLogger(LoginAndRegisterController.class);
-
     private static List<UserSettingInterface> settingInterfaces;
-
 
     @Autowired
     private ForumPasswordService passwordService;
@@ -168,7 +162,7 @@ public class UserSettingsController extends ForumController
     }
 
 
-    private ModelAndView displaySettings(List<UserSettingViewData> viewData, String username, boolean needsConfirm)
+    private ModelAndView displaySettings(List<UserSettingViewData> viewData, String username, List<UserRoleStateData> userRoles, boolean needsConfirm)
     {
         ModelAndView mav = new ModelAndView("usersettings.html");
 
@@ -185,13 +179,76 @@ public class UserSettingsController extends ForumController
         mav.addObject("normalSettings",    normalViewData);
         mav.addObject("showConfirmPassword", needsConfirm && protectedViewData.size() > 0);
         mav.addObject("username", username);
+        mav.addObject("userRoles", userRoles);
 
         return mav;
     }
 
 
+    private List<UserRoleStateData> getUserRoles(ForumUser user)
+    {
+        List<ForumRole> userRoles = user.getRoles();
+        List<UserRoleStateData> ret = new ArrayList<>();
+
+        Set<Long> userRoleIDs = new HashSet<>();
+        for (ForumRole role: userRoles) { userRoleIDs.add(role.getId()); }
+
+
+        List<Object[]> roleData = em.createQuery("SELECT role.name, role.id, role.admin FROM ForumRole role"
+                                                         + " WHERE role.defaultRole = false ORDER BY role.priority DESC", Object[].class)
+                                          .getResultList();
+
+        int i = 0;
+
+        for (Object[] d: roleData)
+        {
+            String  name    = (String)d[0];
+            boolean present = userRoleIDs.contains((Long)d[1]);
+            ret.add(new UserRoleStateData(name, present, (boolean)d[2], i++));
+        }
+
+        return ret;
+    }
+
+
+    private void updateRolesFromParams(ForumUser user, Map<String, String[]> params) throws IllegalArgumentException
+    {
+        Map<String, Boolean> roleStates = new HashMap<>();
+
+        for (Map.Entry<String, String[]> param: params.entrySet())
+        {
+            String roleName  = param.getKey().toLowerCase();
+            String roleState = param.getValue()[0];
+
+            if (roleName.startsWith("role:")) { roleName = roleName.substring("role:".length()); }
+            else { continue; }
+
+            roleStates.put(roleName, "1".equals(roleState));
+        }
+
+        if (roleStates.isEmpty())
+        {
+            throw new IllegalArgumentException("No roles specified");
+        }
+
+        List<ForumRole> roles = em.createQuery("SELECT role FROM ForumRole role WHERE lower(role.name) IN :roleNames"
+                                                       + " AND role.defaultRole = false", ForumRole.class)
+                                        .setParameter("roleNames", roleStates.keySet())
+                                        .getResultList();
+
+        for (ForumRole role: roles)
+        {
+            boolean keep = roleStates.get(role.getName().toLowerCase());
+
+            if (keep) { user.addRole(role); }
+            else      { user.removeRole(role); }
+        }
+    }
+
+
+
     @Transactional
-    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    @RequestMapping(value = "/settings")
     public ModelAndView viewOwnSettings(HttpServletRequest  request,
                                         HttpServletResponse response)
     {
@@ -201,12 +258,14 @@ public class UserSettingsController extends ForumController
         if (errorMAV != null) { return errorMAV; }
 
         List<UserSettingViewData> viewData = buildSettingData(viewUser);
-        return displaySettings(viewData, viewUser.getUsername(), true);
+        List<UserRoleStateData> userRoles  = userHasPermission(viewUser, UserPermission.MANAGE_USERS) ? getUserRoles(viewUser) : null;
+
+        return displaySettings(viewData, viewUser.getUsername(), userRoles, true);
     }
 
 
     @Transactional
-    @RequestMapping(value = "/settings/*", method = RequestMethod.GET)
+    @RequestMapping(value = "/settings/*")
     public ModelAndView viewOtherSettings(HttpServletRequest  request,
                                           HttpServletResponse response)
     {
@@ -219,24 +278,22 @@ public class UserSettingsController extends ForumController
         if (errorMAV != null) { return errorMAV; }
 
         List<UserSettingViewData> viewData = buildSettingData(settingsUser);
+        List<UserRoleStateData> userRoles  = userHasPermission(viewUser, UserPermission.MANAGE_USERS) ? getUserRoles(settingsUser) : null;
         boolean needsConfirm = viewUser.getUsername().equalsIgnoreCase(settingsUsername);
-        return displaySettings(viewData, settingsUser.getUsername(), needsConfirm);
+
+        return displaySettings(viewData, settingsUser.getUsername(), userRoles, needsConfirm);
     }
 
 
     @Transactional
-    @RequestMapping(value = "/savesettings", method = RequestMethod.GET)
-    public ModelAndView waitWhyAreYouTryingToSaveSettingsWithAGETRequestThatIsWrongAndBad(HttpServletRequest request, HttpServletResponse response)
-    {
-        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-        return viewOwnSettings(request, response);
-    }
-
-
-    @Transactional
-    @RequestMapping(value = "/savesettings", method = RequestMethod.POST)
+    @RequestMapping(value = "/savesettings")
     public ModelAndView saveSettings(HttpServletRequest request, HttpServletResponse response)
     {
+        if (!request.getMethod().equals("POST"))
+        {
+            return errorPage("usersettings_error.html", "POST_ONLY", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
         ForumUser viewUser = getUserFromRequest(request);
 
         String    settingsUsername = request.getParameter("username");
@@ -309,12 +366,63 @@ public class UserSettingsController extends ForumController
 
         if (thingsChanged) { userRepository.saveAndFlush(settingsUser); }
 
-        ModelAndView mav = displaySettings(viewData, settingsUser.getUsername(), needsConfirm);
+        List<UserRoleStateData> userRoles  = userHasPermission(viewUser, UserPermission.MANAGE_USERS) ? getUserRoles(settingsUser) : null;
+        ModelAndView mav = displaySettings(viewData, settingsUser.getUsername(), userRoles, needsConfirm);
+
         mav.addObject("savingSettings", true);
         mav.addObject("errorsOccured", errorsOccured);
         mav.addObject("thingsChanged", thingsChanged);
         mav.addObject("missingConfirm", missingConfirm);
 
         return mav;
+    }
+
+
+    // parameter format:
+    //  key: role:[role name]
+    //  value: 0 for "take away this role", 1 for "give this role"
+
+    @Transactional
+    @RequestMapping(value = "/saveuserroles")
+    public ModelAndView saveUserRoles(HttpServletRequest request, HttpServletResponse response)
+    {
+        if (!request.getMethod().equals("POST"))
+        {
+            return errorPage("userroles_error.html", "POST_ONLY", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        ForumUser accessUser = getUserFromRequest(request);
+
+        if (!userHasPermission(accessUser, UserPermission.MANAGE_USERS))
+        {
+            return errorPage("userroles_error.html", "NOT_ALLOWED", HttpStatus.UNAUTHORIZED);
+        }
+
+        String modifyUsername = request.getParameter("username");
+
+        if (modifyUsername == null)
+        {
+            return errorPage("userroles_error.html", "MISSING_USERNAME", HttpStatus.BAD_REQUEST);
+        }
+
+        ForumUser modifyUser = userRepository.findByUsernameIgnoreCase(modifyUsername);
+
+        if (modifyUser == null)
+        {
+            return errorPage("userroles_error.html", "NOT_FOUND", HttpStatus.NOT_FOUND);
+        }
+
+        try
+        {
+            updateRolesFromParams(modifyUser, request.getParameterMap());
+        }
+        catch (IllegalArgumentException e)
+        {
+            return errorPage("userroles_error.html", "MISSING_ROLES", HttpStatus.BAD_REQUEST);
+        }
+
+        ModelAndView ret = new ModelAndView("userroles_saved.html");
+        ret.addObject("username", modifyUser.getUsername());
+        return ret;
     }
 }
